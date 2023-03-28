0411a00dc63e7d272c414bcdd61ac9835f3fef2e
whs820 <whs820@usask.ca>
2023-03-27 20:57:18 -0600

    feature: added secure routings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { verifyAuth } from "./lib/auth";
 
 export async function middleware(req: NextRequest) {
@@ -8,6 +8,16 @@ export async function middleware(req: NextRequest) {
     (await verifyAuth(token).catch((err) => {
       console.log(err);
     }));
+
+  const url = req.url;
+
+  if (!verifiedToken) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (req.url.includes("/channels") && verifiedToken) {
+    return NextResponse.redirect(new URL("/channels"));
+  }
 }
 
 export const config = {
---
+++ src/pages/channels.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export default function Channel() {
+  return <>p</>;
+}
